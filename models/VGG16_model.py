7669f1722b750dd75a29bb989568a3ba3b98fe5e
Suchiralaknath <suchiralaknath@gmail.com>
2023-08-01 09:56:11 +0530

    [wip] got a result, but not accurate. need to be trained

---
+++ models/VGG16_model.py
@@ -0,0 +1,28 @@
+import torch
+#from torch import Module
+import torch.nn as nn
+from torchvision import models, transforms
+import torch.nn.functional as F
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg16', pretrained=True)
+        
+    def forward(self, x):
+        return self.model(x)
+
+class Feature_Extractor(nn.Module):
+    def __init__(self) :
+        super().__init__()
+
+        self.model = Model()
+        self.model = nn.Sequential(*list(self.model.children())[:-1])
+
+    def forward(self, image1, image2):
+        features1 = self.model(image1)
+        features2 = self.model(image2)
+        #features1 = torch.nn.Flatten(features1)
+        #features2 = torch.nn.Flatten(features2)
+        similarity = F.cosine_similarity(features1,features2)
+        return similarity
